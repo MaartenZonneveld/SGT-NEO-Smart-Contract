e4117321516532dcb05106acda2022f0af995d02
Maarten Zonneveld <maartenzonneveld@live.nl>
2018-06-03 13:11:37 +0200

    Fix CurrentSwapRate reverted changes

--- SGT-Smart-Contract/Contracts/Crowdsale.cs
+++ SGT-Smart-Contract/Contracts/Crowdsale.cs
@@ -383,7 +383,7 @@ namespace SGTNEOSmartContract
             }
 
             BigInteger tokenValuePerNEO = CurrentSwapRate(context);
-            BigInteger tokenValueRequested = contributionAmountInNEO * tokenValuePerNEO / Token.TOKEN_DECIMALS_FACTOR;
+            BigInteger tokenValueRequested = contributionAmountInNEO * tokenValuePerNEO;
 
             BigInteger currentlySold = GetTokensSold(context);
             BigInteger newSold = currentlySold + tokenValueRequested;
@@ -480,7 +480,7 @@ namespace SGTNEOSmartContract
             return CROWDSALE_CONTRIBUTED_KEY + address;
         }
 
-        // Swap rate = the amount of SGT you get for 1 NEO (multiplied by token decimal factor)
+        // Swap rate factor = the amount of SGT you get for 1 NEO
         static BigInteger CurrentSwapRate(StorageContext context)
         {
             if (TimeInPrivateSale(context) || TimeInPresale(context))
